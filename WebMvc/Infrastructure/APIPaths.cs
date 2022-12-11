4d422fd7177fe0bd307bbf0c5f450685dc2ff302
Kal <info@kalacademy.org>
2022-12-11 12:23:58 -0800

    Dockerization and web client

---
+++ WebMvc/Infrastructure/APIPaths.cs
@@ -0,0 +1,22 @@
+﻿namespace WebMvc.Infrastructure
+{
+    public static class APIPaths
+    {
+        public static class Catalog
+        {
+            public static string GetAllTypes(string baseUrl)
+            {
+                return $"{baseUrl}/catalogtypes";
+            }
+            public static string GetAllBrands(string baseUrl)
+            {
+                return $"{baseUrl}/catalogbrands";
+            }
+            public static string GetAllCatalogItems(string baseUrl, int page, int take)
+            {
+                return $"{baseUrl}/items?pageIndex={page}&pageSize={take}";
+            }
+
+        }
+    }
+}
